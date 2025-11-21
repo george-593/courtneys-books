3e17d1423772cc6f38661db50b16858b9d22fb2b
george-593 <63100333+george-593@users.noreply.github.com>
2025-11-21 14:02:26 +0000

    feat: setup DB in backend

---
+++ backend/Models/BlogEntry.cs
@@ -0,0 +1,9 @@
+namespace backend.Models
+{
+    public class BlogEntry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,17 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
