3ba5511d53a469031546ae771f824516882f4b3c
george-593 <63100333+george-593@users.noreply.github.com>
2025-12-08 20:53:27 +0000

    Change to use expressjs for backend

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+PORT = 3000;
+BASE_ROUTE = "/api";
+
+const app = express();
+const router = express.Router();
+
+// Base Route
+router.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+// Start Server
+app.use(BASE_ROUTE, router);
+app.listen(PORT, () => {
+	console.log("Server running");
+});
