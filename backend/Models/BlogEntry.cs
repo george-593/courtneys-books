3413b240a771ed92d9a60da52e391509e8cffb0a
george-593 <63100333+george-593@users.noreply.github.com>
2026-02-06 13:44:20 +0000

    Replace c# backend with python

--- backend/Models/BlogEntry.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.Models
-{
-    public class BlogEntry
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-    }
-}
