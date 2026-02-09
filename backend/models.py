e3b2ca570e145da0327bee0168ca4634f168a5d1
george-593 <63100333+george-593@users.noreply.github.com>
2026-02-09 15:18:13 +0000

    Update types in models.py

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import func
+from sqlalchemy import DateTime, String, Text, func
 from datetime import datetime
 from .database import Base
 
@@ -8,9 +8,9 @@ class BlogPost(Base):
     __tablename__ = "blogposts"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column()
-    content: Mapped[str] = mapped_column()
-    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
-        server_default=func.now(), onupdate=func.now()
+        DateTime, server_default=func.now(), onupdate=func.now()
     )
