3aa26e7cd11ea033cb8e7294d5a2eeba502a10ba
george-593 <63100333+george-593@users.noreply.github.com>
2026-02-09 13:10:39 +0000

    feat: Add route for getting one blog post

--- backend/routes/blog_posts.py
+++ backend/routes/blog_posts.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from backend import models, schemas, dependencies
@@ -21,3 +21,14 @@ async def create_blog_post(
 async def get_blog_posts(db: AsyncSession = Depends(dependencies.get_db)):
     result = await db.execute(select(models.BlogPost))
     return result.scalars().all()
+
+
+@router.get("/{post_id}", response_model=schemas.BlogPostOut)
+async def get_blog_post(post_id: int, db: AsyncSession = Depends(dependencies.get_db)):
+    result = await db.execute(
+        select(models.BlogPost).where(models.BlogPost.id == post_id),
+    )
+    post = result.scalar_one_or_none()
+    if post is None:
+        raise HTTPException(status_code=404, detail="Blog post not found")
+    return post
