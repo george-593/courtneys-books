f22c0101bdeeb9c82f0ca68b021a9c5eef70067a
george-593 <63100333+george-593@users.noreply.github.com>
2026-02-09 12:30:18 +0000

    feat: change models.py to include created_at and updated_at

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import func
+from datetime import datetime
 from .database import Base
 
 
@@ -8,3 +10,7 @@ class BlogPost(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column()
     content: Mapped[str] = mapped_column()
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
+    )
